90e0969e61ef79294d4b7c93a9e1d1a03343a554
yuleeuz <84665351+yuleeuz@users.noreply.github.com>
2021-09-09 17:57:07 +0200

    Baumeln

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-function revealMessage() {
-	document.getElementById("hiddenMessage").style.display = 'block';
-}
+
+const Schwingen = gsap.timeline(
+	{
+		paused: true
+	}
+)
+
+Schwingen
+	.to(
+		'#Emblem',
+		{
+			duration: 5, rotation: '360', ease: 'elastic'
+		}
+	)
+
+const EmblemAnstossen = document.getElementById('Emblem');
+
+EmblemAnstossen.addEventListener('mouseover', () => {
+	Schwingen.play();
+	Schwingen.restart();
+})
+EmblemAnstossen.addEventListener('touchstart', () => {
+	Schwingen.play();
+	Schwingen.restart();
+})
