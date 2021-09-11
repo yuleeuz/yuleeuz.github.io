2a99ab224bacaae42e1f56b1adf054ed140cd971
yuleeuz <84665351+yuleeuz@users.noreply.github.com>
2021-09-11 17:27:39 +0200

    Intro

--- js/script.js
+++ js/script.js
@@ -2,13 +2,34 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Draggable);
 
 
-const Rad = document.getElementById('Rad');
 const Emblem = document.getElementById('Emblem');
+
+const Willkommen = document.getElementById('Willkommen');
+
+const Rad = document.getElementById('Rad');
 const Griff = document.getElementById('Griff');
 
-Emblem.style.display = 'none';
 
-/*
+
+Emblem.style.opacity = 0;
+Willkommen.style.opacity = 0;
+
+Rad.style.opacity = 0;
+
+let Breite = Rad.clientWidth;
+Griff.style.height = Breite + 'px';
+Griff.style.width = Breite + 'px';
+
+
+
+window.addEventListener('load', function() {
+
+    window.scrollTo(0,500);
+    console.log('Begruessung beginnt');
+    Begruessung.play();
+
+})
+
 const Begruessung = gsap.timeline(
 	{
 		paused: true
@@ -16,33 +37,40 @@ const Begruessung = gsap.timeline(
 )
 
 Begruessung
-	.to( 
-		['#Emblem', '#Rad'], {
-			opacity: 0
-		}
-		)
+	.to(
+		'#Emblem', {
+			opacity: 1,
+			duration: 1,
+		} )
+	.to(
+		'#Emblem', {
+			opacity: 0,
+			duration: 1,
+		} )
+	.to(
+		'#Willkommen', {
+			opacity: 1,
+			delay: 1,
+			onComplete: function() {
+				Willkommen.play();
+			}
+		} )
 	.to(
 		'#Willkommen', {
 			opacity: 0,
-			delay: 3
-		}
-		)
+			delay: 3.5
+		} )
 	.to( 
-		['#Emblem', '#Rad'], {
+		'#Rad', {
 			opacity: 1,
-			duration: 1
-		}
-		)
+			duration: 2
+		} )
 
-window.addEventListener('load', () => {
-    Begruessung.play();
-});
 
-*/
 
-let Breite = Rad.clientWidth;
-Griff.style.height = Breite + 'px';
-Griff.style.width = Breite + 'px';
+
+
+
 
 let Titel = 0; // 0 DIGITAL, 1 MATERIAL, 2 MENTAL
 
@@ -51,63 +79,51 @@ let Ausrichtung = 0; // Grad des Rades
 let Widerstand = 0; // Grad gegen Widerstand akuter Drehung
 
 const Auswahl = Draggable.create(
-
 	'#Griff',{
 		type: 'rotation',
-
 		onDrag: function() {
+
 			Drehung = this.rotation;
 
 			if( Drehung < 60 && Drehung > 0 ) {
-
-
 				Widerstand = Ausrichtung + 2 * Math.log(Drehung);
-				console.log(Widerstand);
-
 				gsap.to( '#Rad', {  rotation: Widerstand  } );
-
 			} if ( Drehung > -60 && Drehung < 0 ) {
-
 				Widerstand = Ausrichtung + -2 * Math.log(-Drehung);
-				console.log(Widerstand);
-
 				gsap.to( '#Rad', {  rotation: Widerstand  } );
 
 			} if( Drehung > 60 || Drehung < -60 ) {
 
-				this.endDrag();
-				gsap.to( '#Griff', {  rotation: 0  } );
+			this.endDrag();
+			gsap.to( '#Griff', {  rotation: 0  } );
 
-				if( Drehung < 0 ){		
-					if( Titel == 0 ){  Ausrichtung -= 110;  }
-					if( Titel == 1 ){  Ausrichtung -= 125;  }
-					if( Titel == 2 ){  Ausrichtung -= 125;  }
-	
-					Titel = (Titel + 2 )%3;
+			if( Drehung < 0 ){		
+				if( Titel == 0 ){  Ausrichtung -= 110;  }
+				if( Titel == 1 ){  Ausrichtung -= 125;  }
+				if( Titel == 2 ){  Ausrichtung -= 125;  }
+				Titel = (Titel + 2 )%3;
 
-				} else {
-					if( Titel == 0 ){  Ausrichtung += 125;  }
-					if( Titel == 1 ){  Ausrichtung += 125;  }
-					if( Titel == 2 ){  Ausrichtung += 110;  }
-	
-					Titel = (Titel + 1)%3;
-				}
+			} else {
+				if( Titel == 0 ){  Ausrichtung += 125;  }
+				if( Titel == 1 ){  Ausrichtung += 125;  }
+				if( Titel == 2 ){  Ausrichtung += 110;  }
+				Titel = (Titel + 1)%3;
+			}
 
-				gsap.to( '#Rad', {  rotation: Ausrichtung  } );
-				Widerstand = Ausrichtung;
+			gsap.to( '#Rad', {  rotation: Ausrichtung  } );
+			Widerstand = Ausrichtung;
 
 				console.log('Gedreht ' + 'A ' + Ausrichtung + ' T ' +  Titel + ' R ' +  this.rotation + ' W ' +  Widerstand);
 			}
-		},
-
-		onRelease: function() {
-			gsap.to( '#Rad', {  rotation: Ausrichtung  } );			
-
+		}, onRelease: function() {
+	
 			gsap.to( '#Griff', {  rotation: 0  } );
 
+			gsap.to( '#Rad', {  rotation: Ausrichtung  } );		
 			Widerstand = Ausrichtung;
+
 				console.log('Losgelassen ' + 'A ' + Ausrichtung + ' T ' +  Titel + ' R ' +  this.rotation + ' W ' +  Widerstand);
-		}
+			}
 		
 	}
 );
@@ -117,28 +133,5 @@ const Auswahl = Draggable.create(
 
 
 
-/*
-const Schwingen = gsap.timeline(
-	{
-		paused: true
-	}
-)
 
-Schwingen
-	.to(
-		'#Emblem',
-		{
-			duration: 5, rotation: '360', ease: 'elastic'
-		}
-	)
 
-const EmblemAnstossen = document.getElementById('Emblem');
-
-EmblemAnstossen.addEventListener('mouseover', () => {
-	Schwingen.play();
-	Schwingen.restart();
-})
-EmblemAnstossen.addEventListener('touchstart', () => {
-	Schwingen.play();
-	Schwingen.restart();
-})*/
