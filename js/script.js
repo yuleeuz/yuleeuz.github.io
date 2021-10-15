db5135b6e80a8f977cdceb93e983806975443bae
yuleeuz <84665351+yuleeuz@users.noreply.github.com>
2021-10-15 21:23:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ window.addEventListener( 'resize', function() {
 	GriffBereiten();  } );
 
 window.addEventListener( 'wheel', function() {  Input(); });
-window.addEventListener( 'click', function() {  Input(); });
+window.addEventListener( 'touchstart', function() {  Input(); });
 
 
 
